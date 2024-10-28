f692f4f85e9e45188d380076aee7a7f9b0fa11b1
Lucas Campos <lucas.c_oliveira@live.com>
2024-10-28 12:59:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
+		Seller sl = new Seller(1, "Lucas","lucas@gmail.com", new Date(), 3000, obj);
+		System.out.println(sl);
 
 	}
 
